8450bda4282119ddad878a6ff9668803f2187421
Pranav Srivastav <109237040+iamransh@users.noreply.github.com>
2024-07-21 23:05:17 +0530

    added view pdf feature

--- stock_news_app_frontend/lib/_components/posts.dart
+++ stock_news_app_frontend/lib/_components/posts.dart
@@ -7,8 +7,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stock_news_app_frontend/_components/Comments.dart';
 import 'package:http/http.dart' as http;
 import 'package:stock_news_app_frontend/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class Posts extends StatefulWidget {
-  const Posts({super.key,});
+  const Posts({
+    super.key,
+  });
 
   @override
   State<Posts> createState() => _PostsState();
@@ -27,81 +31,90 @@ class _PostsState extends State<Posts> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.sizeOf(context).width;
     double screenHeight = MediaQuery.sizeOf(context).height;
-    void handleLike()async{
+    void handleLike() async {
       setState(() {
-        if(isLiked == true){
-            isLiked = false;
-            numLikes-=1;
-          
-        }
-        else{
-            isLiked = true;
-            numLikes+=1;
-            if(isDisliked){
-              numDislikes-=1;
-              isDisliked = false;
-            }
+        if (isLiked == true) {
+          isLiked = false;
+          numLikes -= 1;
+        } else {
+          isLiked = true;
+          numLikes += 1;
+          if (isDisliked) {
+            numDislikes -= 1;
+            isDisliked = false;
+          }
         }
       });
-      Uri likeUri = Uri.parse(baseUrl+'post/like');
+      Uri likeUri = Uri.parse(baseUrl + 'post/like');
       final req = jsonEncode({});
-      final response = await client.post(likeUri, body: req, headers: {
-        'Content-Type': 'application/json'
-      });
+      final response = await client.post(likeUri,
+          body: req, headers: {'Content-Type': 'application/json'});
       final res = jsonDecode(response.body);
-      if (res['status']==false){
-      setState(() {
-        if(isLiked == false){
+      if (res['status'] == false) {
+        setState(() {
+          if (isLiked == false) {
             isLiked = true;
-            numLikes+=1;
-          
-        }
-        else{
+            numLikes += 1;
+          } else {
             isLiked = false;
-            numLikes-=1;
-        }
-      });
+            numLikes -= 1;
+          }
+        });
       }
     }
-    void handleDislike()async{
-            setState(() {
-        if(isDisliked == true){
-            isDisliked = false;
-            numDislikes-=1;
-        }
-        else{
-            isDisliked = true;
-            numDislikes+=1;
-            if(isLiked){
-              isLiked = false;
-              numLikes -= 1;
-            }
-        }
-      });
-        Uri likeUri = Uri.parse(baseUrl+'post/dislike');
-      final req = jsonEncode({});
-      final response = await client.post(likeUri, body: req, headers: {
-        'Content-Type': 'application/json'
-      });
-      final res = jsonDecode(response.body);
-      if(res['status']==false){
-                    setState(() {
-        if(isDisliked == false){
-            isDisliked = true;
-            numDislikes+=1;
-        }
-        else{
-            isDisliked = false;
-            numDislikes-=1;
-        }
-      });
-      }
 
+    void handleDislike() async {
+      setState(() {
+        if (isDisliked == true) {
+          isDisliked = false;
+          numDislikes -= 1;
+        } else {
+          isDisliked = true;
+          numDislikes += 1;
+          if (isLiked) {
+            isLiked = false;
+            numLikes -= 1;
+          }
+        }
+      });
+      Uri likeUri = Uri.parse(baseUrl + 'post/dislike');
+      final req = jsonEncode({});
+      final response = await client.post(likeUri,
+          body: req, headers: {'Content-Type': 'application/json'});
+      final res = jsonDecode(response.body);
+      if (res['status'] == false) {
+        setState(() {
+          if (isDisliked == false) {
+            isDisliked = true;
+            numDislikes += 1;
+          } else {
+            isDisliked = false;
+            numDislikes -= 1;
+          }
+        });
+      }
     }
-    Future? handleCommentSectionOpen(){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>CommentsScreen(postId: "",)));
+
+    Future? handleCommentSectionOpen() {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => CommentsScreen(
+                    postId: "",
+                  )));
     }
-    Function? handlePdf(){}
+
+    void handlePdf() async {
+      const Pdfurl =
+          "https://nsearchives.nseindia.com/corporate/xbrl/NESCO_01012024014219_CIM_15614_1005823_01012024014219_WEB.xml";
+      final Uri url = Uri.parse(Pdfurl);
+      if (await canLaunchUrl(url)) {
+        await launchUrl(url, mode: LaunchMode.externalApplication);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+
     var postActions = [
       {
         'icon': 'assets/Bullish.svg',
@@ -139,7 +152,9 @@ class _PostsState extends State<Posts> {
 
     return Container(
       padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.symmetric(vertical: 5,),
+      margin: const EdgeInsets.symmetric(
+        vertical: 5,
+      ),
       decoration: BoxDecoration(
         border: Border.all(width: 1, color: const Color(0x99515151)),
         borderRadius: BorderRadius.circular(20),
@@ -161,7 +176,10 @@ class _PostsState extends State<Posts> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => CompanyProfile(id: '', isFollowing: true,),
+                      builder: (context) => CompanyProfile(
+                        id: '',
+                        isFollowing: true,
+                      ),
                     ),
                   );
                 },
@@ -189,7 +207,6 @@ class _PostsState extends State<Posts> {
               ),
             ],
           ),
-          
           Container(
             margin: EdgeInsets.symmetric(vertical: 10),
             decoration: const BoxDecoration(
@@ -233,47 +250,51 @@ class _PostsState extends State<Posts> {
                   ]),
             ),
           ),
-Row(
-  mainAxisAlignment: MainAxisAlignment.spaceAround,
-  children: postActions.map((action) {
-    final String icon = action['icon'] as String;
-    final String label = action['label'] as String;
-    final int? num = action['num'] as int?;
-    final VoidCallback handleClick = action['handleClick'] as VoidCallback;
-    final bool isActive = action['isActive'] as bool;
-    final Color activeColor = action['activeColor'] as Color;
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: postActions.map((action) {
+              final String icon = action['icon'] as String;
+              final String label = action['label'] as String;
+              final int? num = action['num'] as int?;
+              final VoidCallback handleClick =
+                  action['handleClick'] as VoidCallback;
+              final bool isActive = action['isActive'] as bool;
+              final Color activeColor = action['activeColor'] as Color;
 
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: handleClick,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [ 
-          Column(
-          children: [
-            SvgPicture.asset(
-              icon,
-              semanticsLabel: label,
-              colorFilter: ColorFilter.mode(isActive?activeColor: Colors.white, BlendMode.srcIn),
-            ),
-            SizedBox(height: 5,),
-              Text(
-                '$label',
-                style: const TextStyle(color: Colors.white),
-              ),
-          ],
-        ),
-                  if (num != null)
-            Text(
-              '$num',
-              style: const TextStyle(color: Colors.white),
-            ),
-
-        ],
-      ),
-    );
-  }).toList(),
-),
+              return GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: handleClick,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        SvgPicture.asset(
+                          icon,
+                          semanticsLabel: label,
+                          colorFilter: ColorFilter.mode(
+                              isActive ? activeColor : Colors.white,
+                              BlendMode.srcIn),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          '$label',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    if (num != null)
+                      Text(
+                        '$num',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
